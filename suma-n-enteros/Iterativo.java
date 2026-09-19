8622ee83ddedebf1ad0833134414bde37b0a2d77
sa1ntb3lts <229399078+sa1ntb3lts@users.noreply.github.com>
2026-09-19 16:10:52 -0700

    Se ha finalizado la implementacion del archivo

--- suma-n-enteros/Iterativo.java
+++ suma-n-enteros/Iterativo.java
@@ -1,5 +1,12 @@
 public class Iterativo {
     public static void main(String[] args) {
-        
+        int n = 5;
+        int suma = 0;
+
+        for (int i = 1; i <= n; i++) {
+            suma += i;
+        }
+
+        System.out.println(suma);
     }
 }
