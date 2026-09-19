f965d072b2571d8c4eb319baed28f5f79b325f8a
sa1ntb3lts <229399078+sa1ntb3lts@users.noreply.github.com>
2026-09-19 14:53:47 -0700

    Comentarios finales anadidos

--- numero-menor-arreglo/NumeroMenor.java
+++ numero-menor-arreglo/NumeroMenor.java
@@ -18,3 +18,21 @@ public class NumeroMenor {
         System.out.println("Valor minimo en el arreglo: " + valorMinimo);
     }
 }
+
+/*
+Lo unico que se modifico en el codigo fueron dos partes:
+
+1.
+Codigo mejorado: int[] A = {2, 5, 6, 1};
+Codigo original: int[n] A = {...};
+
+> Para lograr que el codigo se viera exactamente como lo escribimos y que a su vez funcionara, tendriamos
+  que agregar Scanner y otras validaciones que extenderian demasiado el codigo, por eso optamos por esta solucion.
+
+2.
+Codigo mejorado: for(int i = 1; i < A.length; i++)
+Codigo original: for(int i = 1; i < n; i++)
+
+> Como decidimos excluir la variable n del codigo, tuvimos que optar por esta solucion la cual era la mas sencilla
+  y rapida.
+*/
