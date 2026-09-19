06f0cf127b8bd7777d3bf2445c4f44880724cd60
sa1ntb3lts <229399078+sa1ntb3lts@users.noreply.github.com>
2026-09-19 16:50:33 -0700

    Comentarios anadidos

--- suma-n-enteros/Iterativo.java
+++ suma-n-enteros/Iterativo.java
@@ -3,6 +3,8 @@ public class Iterativo {
         int n = 5;
         int suma = 0;
 
+        // El valor de i va a incrementar de uno en uno hasta llegar a n,
+        // y se va a ir sumando al valor de la variable 'suma'.
         for (int i = 1; i <= n; i++) {
             suma += i;
         }
