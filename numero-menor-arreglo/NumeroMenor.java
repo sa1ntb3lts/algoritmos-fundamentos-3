0bdd61430dbd49c7a906db2a136c16e9f28b75f1
sa1ntb3lts <229399078+sa1ntb3lts@users.noreply.github.com>
2026-09-19 14:29:58 -0700

    Se ha finalizado la implementacion

--- numero-menor-arreglo/NumeroMenor.java
+++ numero-menor-arreglo/NumeroMenor.java
@@ -1,5 +1,14 @@
 public class NumeroMenor {
     public static void main(String[] args) {
-        
+        int[] A = {2, 5, 6, 1};
+        int valorMinimo = A[0];
+
+        for(int i = 1; i < 4; i++) {
+            if (A[i] < valorMinimo) {
+                valorMinimo = A[i];
+            }
+        }
+
+        System.out.println("Valor minimo en el arreglo: " + valorMinimo);
     }
 }
