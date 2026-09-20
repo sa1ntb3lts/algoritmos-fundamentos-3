db8a9fb016aec9243d565c7c27c4ff77ccc15b69
sa1ntb3lts <229399078+sa1ntb3lts@users.noreply.github.com>
2026-09-19 17:34:05 -0700

    Implementacion finalizada

--- suma-n-enteros/Recursivo.java
+++ suma-n-enteros/Recursivo.java
@@ -1,7 +1,15 @@
 public class Recursivo {
     public static void main(String[] args) {
+        long resultado = algoritmo(1);
         
+        System.out.println(resultado);
     }
 
-    
+    public static long algoritmo(long n) {
+        if (n == 0) {
+            return 0;
+        } else {
+            return n + algoritmo(n - 1);
+        }
+    }
 }
