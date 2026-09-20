0ba3d8333109327764e02fd08db87dcc639bcd32
sa1ntb3lts <229399078+sa1ntb3lts@users.noreply.github.com>
2026-09-19 18:21:31 -0700

    Se borro un comentario del programa

--- numero-menor-arreglo/NumeroMenor.java
+++ numero-menor-arreglo/NumeroMenor.java
@@ -1,12 +1,6 @@
 public class NumeroMenor {
     public static void main(String[] args) {
         int[] A = {2, 5, 6, 1};
-
-        // Se le asigna la posicion 0 para que tome en cuenta TODOS los valores
-        // que hay en el arreglo. Si lo iniciaramos desde la posicion 2, la
-        // comparacion no va a funcionar como se esperaria, ya que se va a 
-        // comparar con los valores que le preceden sin tomar en cuenta los que
-        // le anteceden, los cuales pueden ser el valor minimo en el arreglo.
         int valorMinimo = A[0];
 
         for(int i = 1; i < A.length; i++) {
