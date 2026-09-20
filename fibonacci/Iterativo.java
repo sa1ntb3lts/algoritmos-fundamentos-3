265f036e4ee7257c2e144d1c3e4e7891b0cfdd29
sa1ntb3lts <229399078+sa1ntb3lts@users.noreply.github.com>
2026-09-19 18:18:23 -0700

    Se modifico la implementacion

--- fibonacci/Iterativo.java
+++ fibonacci/Iterativo.java
@@ -17,3 +17,20 @@ public class Iterativo {
         System.out.println(sumatoria);
     }
 }
+
+/*
+Cambios en el programa:
+
+long fibonacci(int n) {
+    int i = 0;
+    int j = 1;                      <------ Codigo original.
+    int sumatoria = 0;
+    for(k = 0; k <= n; k++) {
+        j += i;
+    }
+}
+
+> Primero anadimos la variable n, luego modificamos el valor de k (por la razon que se menciona
+en el comentario), y cambiamos la logica dentro del for, porque con lo que teniamos el programa
+solamente daba 1 como output, o sea que no estaba avanzando.
+*/
