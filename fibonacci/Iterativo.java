eb753fb591dcac8bc1fe4a158f7a93ca0e7baae1
sa1ntb3lts <229399078+sa1ntb3lts@users.noreply.github.com>
2026-09-19 17:58:22 -0700

    Se finalizo la implementacion del archivo

--- fibonacci/Iterativo.java
+++ fibonacci/Iterativo.java
@@ -1,5 +1,18 @@
+package fibonacci;
+
 public class Iterativo {
     public static void main(String[] args) {
-        
+        int i = 0;
+        int j = 1;
+        int sumatoria = 0;
+        int n = 8;
+
+        for(int k = 1; k <= n; k++) {
+            sumatoria += i;
+            i = j;
+            j = sumatoria;
+        }
+
+        System.out.println(sumatoria);
     }
 }
